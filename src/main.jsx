4e41875eba1cc9b4173f1855d250c66600b80176
werkbon-alt <werkbon@houvast-ontzorgen.net>
2026-05-27 09:53:25 +0200

    Remove App function from main.jsx
    Removed the App function definition from main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Houvast Werkbon V2 🚀</h1>
-      <p>De nieuwe professionele werkbon-app draait.</p>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
