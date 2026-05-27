cba08f3fbef782316b76144e8f06bc92cbbbebe5
werkbon-alt <werkbon@houvast-ontzorgen.net>
2026-05-27 12:10:11 +0200

    Delete src/src/supabase.js

--- src/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  "https://pwmlnlmqnjqntfchqfbx.supabase.co",
-  "sb_publishable_WH0zAMdPqIPk3loonvN0ig_0GEjhkj_"
-);
