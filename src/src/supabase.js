fb12545904f2e46c417a5712b4e4fff3e1744378
werkbon-alt <werkbon@houvast-ontzorgen.net>
2026-05-27 11:46:56 +0200

    Add Supabase client initialization

---
+++ src/src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://pwmlnlmqnjqntfchqfbx.supabase.co",
+  "sb_publishable_WH0zAMdPqIPk3loonvN0ig_0GEjhkj_"
+);
